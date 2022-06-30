c67212b080f5353f09915d6dd1bf495102145bdb
ISHIKA TRIVEDI <ishika.trivedi2020@vitstudent.ac.in>
2022-06-30 13:33:19 +0530

    Initial Commit

---
+++ Android/Poetry_App/app/src/main/java/com/example/poetry_app/Adapter/PoetryAdapter.java
@@ -0,0 +1,17 @@
+package com.example.poetry_app.Adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PoetryAdapter {
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
