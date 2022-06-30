c26bd5451c2ed5371b59b612ce190807d9b9e0a5
ISHIKA TRIVEDI <ishika.trivedi2020@vitstudent.ac.in>
2022-06-30 19:44:07 +0530

    Adapter.java class added

--- Android/Poetry_App/app/src/main/java/com/example/poetry_app/Adapter/PoetryAdapter.java
+++ Android/Poetry_App/app/src/main/java/com/example/poetry_app/Adapter/PoetryAdapter.java
@@ -1,17 +1,66 @@
+// Constructor, ViewHolder, 3 methods:- onCreateViewHolder, onBindViewHolder, getItemCount
+
 package com.example.poetry_app.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PoetryAdapter {
+import com.example.poetry_app.Models.PoetryModel;
+import com.example.poetry_app.R;
 
+import java.util.List;
+
+public class PoetryAdapter extends RecyclerView.Adapter<PoetryAdapter.ViewHolder>{
+    Context context;
+    List<PoetryModel> poetryModels;
+
+    public PoetryAdapter(Context context, List<PoetryModel> poetryModels) {
+        this.context = context;
+        this.poetryModels = poetryModels;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+       View view = LayoutInflater.from(context).inflate(R.layout.poetry_list_design,null);
+       return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.poetName.setText(poetryModels.get(position).getPoet_name());
+        holder.poetry.setText(poetryModels.get(position).getPoetry_data());
+        holder.date_time.setText(poetryModels.get(position).getDate_time());
+    }
+
+    @Override
+    public int getItemCount() {
+        return poetryModels.size();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView poetry,poetName,date_time;
+        AppCompatButton updateBtn,deleteBtn;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            poetName = itemView.findViewById(R.id.textView_poetName);
+            poetry = itemView.findViewById(R.id.textView_poetryData);
+            date_time = itemView.findViewById(R.id.textView_poetryDateTime);
+
+            updateBtn = itemView.findViewById(R.id.update_btn);
+            deleteBtn = itemView.findViewById(R.id.delete_btn);
         }
     }
 }
